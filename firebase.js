a17e454b99bf633d702d183f46c3416e8ebb6da3
islam100 <70288544+islam100@users.noreply.github.com>
2025-08-10 20:49:04 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// firebase.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCFgRQxK_W2T9S5QWZnxAd-kYV52Kzyb94",
+  authDomain: "zara-life-care.firebaseapp.com",
+  projectId: "zara-life-care",
+  storageBucket: "zara-life-care.appspot.com", // <- यहाँ सही किया गया
+  messagingSenderId: "307306937129",
+  appId: "1:307306937129:web:a949207fe64fe87dd3d538"
+};
